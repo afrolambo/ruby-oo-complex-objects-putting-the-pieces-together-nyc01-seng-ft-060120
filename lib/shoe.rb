15bd10e7340aa23dfd2cac14fd4e3c42c316f4a0
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-06-03 15:38:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,14 @@
 
 class Shoe 
   attr_accessor :brand 
-  attr_reader :color 
+  attr_reader 
   def initialize(brand)
     @brand = brand 
   end 
   
+  def color=(color)
+    @color = color 
+  end 
 
   
 end 
