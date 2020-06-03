bf57bce1991b6b91b9c9e0db28f5e173a470370b
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-06-03 15:43:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,10 @@
 # Make your shoe class here
 
 class Shoe 
-  attr_accessor :brand, :color, :size, :material
+  attr_accessor :brand, :color, :size, :material, :conditon
   attr_reader 
   def initialize(brand)
     @brand = brand 
-  end 
-  
-  def condition=(condition)
-    @condition = condition
   end 
   
 end 
